0388b272887b0c18abd32cb6fadc3b540fbc11d1
Changyeol Choe <duf325@naver.com>
2025-07-20 12:44:23 +0900

    프로젝트 초기 세팅 (#2)
    * feat: 초기 프로젝트 생성
    
    * feat: gitbook api문서 자동화
    
    * fix: 문서 자동화 npm cache 제거
    
    * fix: node js 안정성 문제 해결
    
    * fix: nodejs 호환 문제 해결
    
    * fix: actions flow 간단하게 수정
    
    * fix: nodejs 20으로 업그레이드
    
    * feat: clone 레포에서 문서 배포 프로세스 제거
    
    * docs: 기본적인 README 작성
    
    * feat: 임시로 시큐리티 설정 비활성화
    
    * config: swagger 버전 업그레이드
    
    * feat: 공통 예외 응답 처리 추가
    
    * feat: 회원가입 예시 api 추가
    
    * chore: 불필요한 주석 제거
    
    * chore: editor 설정 파일 git ignore 추가

---
+++ src/main/java/com/withquery/webide_spring_be/domain/user/service/UserService.java
@@ -0,0 +1,9 @@
+package com.withquery.webide_spring_be.domain.user.service;
+
+import com.withquery.webide_spring_be.domain.user.dto.UserRegistrationRequest;
+import com.withquery.webide_spring_be.domain.user.dto.UserRegistrationResponse;
+
+public interface UserService {
+
+    UserRegistrationResponse registerUser(UserRegistrationRequest request);
+} 
---
+++ src/test/java/com/withquery/webide_spring_be/WebideSpringBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withquery.webide_spring_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebideSpringBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
